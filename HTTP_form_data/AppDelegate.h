9b8e2dd2e953dfa8a1334b574cc71868cfd377ba
xlsn0w <xlsn0wios@gmail.com>
2018-07-03 17:06:36 +0800

    add

---
+++ HTTP_form_data/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTTP_form_data
+//
+//  Created by HL on 2018/7/3.
+//  Copyright © 2018年 XL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
